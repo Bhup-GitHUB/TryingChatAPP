3eaadcbd79e446fe31d9b4b48d44fd34db63dde3
Bhup-GitHUB <bkumar_be23@thapar.edu>
2023-10-18 16:44:03 +0530

    1st Commit

--- app/src/main/java/com/example/chatapp/login.kt
+++ app/src/main/java/com/example/chatapp/login.kt
@@ -8,13 +8,16 @@ import android.widget.EditText
 class login : AppCompatActivity() {
     private  lateinit var  edtEmail: EditText
     private  lateinit var  edtpassword: EditText
-    private  lateinit var  btnlogin: Button
+    private lateinit var  btnlogin: Button
     private  lateinit var  btnsignup: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        e
+        edtEmail = findViewById(R.id.edit_email)
+        edtpassword = findViewById(R.id.edit_password)
+        btnsignup = findViewById(R.id.btn_signup)
+        btnlogin =findViewById(R.id.edit_login)
 
 
 
