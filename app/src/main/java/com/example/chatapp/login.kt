e800beee6cdde4e9492d0a2d488da26895623890
Bhup-GitHUB <bkumar_be23@thapar.edu>
2023-10-18 16:27:52 +0530

    1st Commit

--- app/src/main/java/com/example/chatapp/login.kt
+++ app/src/main/java/com/example/chatapp/login.kt
@@ -2,10 +2,26 @@ package com.example.chatapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class login : AppCompatActivity() {
+    private  lateinit var  edtEmail: EditText
+    private  lateinit var  edtpassword: EditText
+    private  lateinit var  btnlogin: Button
+    private  lateinit var  btnsignup: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        e
+
+
+
+
+
+
+
+
     }
 }
